2257daa024d98765e3e2716999a2d30f962fe9ef
shahidaLucky <shahidakawsar@gmail.com>
2019-05-04 20:11:00 +0600

    Initial Commit.

---
+++ src/main/java/bnym/casestudy/CaseStudyApi/CaseStudyApiApplication.java
@@ -0,0 +1,13 @@
+package bnym.casestudy.CaseStudyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseStudyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseStudyApiApplication.class, args);
+	}
+
+}
